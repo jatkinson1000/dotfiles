2665b1b7efaa7c30f63f047f1127cbbd98374d33
Jack Atkinson <jack.atkinson1000@gmail.com>
2023-10-08 22:37:39 +0100

    Add wezterm config.

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,38 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- Color scheme:
+-- config.color_scheme = 'AdventureTime'
+
+config.colors = {
+    -- Default text color
+    foreground = '#00FF00',
+    -- Default background color
+    background = 'black',
+
+    cursor_bg = '#00FF00',
+    cursor_fg = 'black',
+
+    selection_fg = 'black',
+    selection_bg = '#00ff00',
+
+    split = '#444444',
+}
+
+config.cursor_blink_rate = 800
+
+config.window_background_opacity = 0.75
+
+-- and finally, return the configuration to wezterm
+return config
