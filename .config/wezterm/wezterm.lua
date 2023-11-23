e19c6ed2b415882e3212c3fecc0fb189a8bd74af
jatkinson1000 <109271713+jatkinson1000@users.noreply.github.com>
2023-11-23 09:22:20 +0000

    Add a simplified wezterm config for carpentries.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -12,31 +12,8 @@ end
 
 -- This is where you actually apply your config choices
 
--- Color scheme:
--- config.color_scheme = 'AdventureTime'
-
-config.colors = {
-    -- Default text color
-    foreground = '#00FF00',
-    -- Default background color
-    background = 'black',
-
-    cursor_bg = '#00FF00',
-    cursor_fg = 'black',
-
-    selection_fg = 'black',
-    selection_bg = '#00ff00',
-
-    split = '#444444',
-}
-
-config.cursor_blink_rate = 800
-
-config.window_background_opacity = 0.75
-
--- Map option/alt keys so left has composition effects (AltGr) and right does not
-config.send_composed_key_when_left_alt_is_pressed = true
-config.send_composed_key_when_right_alt_is_pressed = false
+-- For example, changing the color scheme:
+config.color_scheme = 'Atelier Cave Light (base16)'
 
 -- and finally, return the configuration to wezterm
 return config
