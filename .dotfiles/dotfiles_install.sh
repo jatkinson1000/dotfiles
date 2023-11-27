49869a294699a69aed51a171809e535f52294ca9
jatkinson1000 <109271713+jatkinson1000@users.noreply.github.com>
2023-11-27 08:13:08 +0000

    Update install script to avoid redundant file creation.

--- .dotfiles/dotfiles_install.sh
+++ .dotfiles/dotfiles_install.sh
@@ -4,14 +4,13 @@ git clone --bare git@github.com:jatkinson1000/dotfiles.git $HOME/.dotfiles
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 dotfiles config --local status.showUntrackedFiles no
 
-mkdir -p .dotfiles-backup
-
 # Try checking out git repo. If fails, back up conflicts.
 dotfiles checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
 else
   echo "Backing up pre-existing dot files.";
+  mkdir -p .dotfiles-backup
   # combine stderr and stdout | select whitespace followed by a dot | awk to print first arg | get 
   dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs dirname | xargs -I{} mkdir -p .dotfiles_backup/{}
   dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles_backup/{}
