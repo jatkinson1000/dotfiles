f4616d232c1e2287542948c241c88a3599fc9390
Jack Atkinson <jack.atkinson1000@gmail.com>
2026-05-24 11:55:01 +0100

    Update nvim clangd lsp config to set filetypes and root markers. Previously it was being triggered on inappropriate filetypes.

--- .config/nvim/lua/lsp/clang.lua
+++ .config/nvim/lua/lsp/clang.lua
@@ -5,6 +5,13 @@
 vim.lsp.config("clangd", {
   -- on_attach = on_attach,
   cmd = {'clangd', '--background-index', '--clang-tidy', '--log=verbose'},
+  filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+  root_markers = {
+    "compile_commands.json",
+    "compile_flags.txt",
+    ".clangd",
+    ".git",
+  },
   init_options = {
     fallbackFlags = { '-std=c++17' },
   },
